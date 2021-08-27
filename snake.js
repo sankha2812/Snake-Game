3a12c21b239f538d5e03615c2ff8f4dbea2fad08
Sankha <sankha2812@gmail.com>
2021-08-27 11:19:04 +0530

    completed setting movement input

--- snake.js
+++ snake.js
@@ -1,8 +1,19 @@
+import { getInputDirection } from "./input.js"
+
 export const SNAKE_SPEED = 1
-const snakeBody = [{ x: 11, y: 11 }]
+const snakeBody = [
+    { x: 11, y: 11 }
+]
 
 export function update() {
     // console.log('update snake')
+    const inputDirection = getInputDirection()
+    for(let i = snakeBody.length - 2; i>=0; i--){
+        snakeBody[i+1] = {...snakeBody[i]}
+    }
+
+    snakeBody[0].x += inputDirection.x
+    snakeBody[0].y += inputDirection.y
 }
 
 

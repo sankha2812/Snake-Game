feb07641f6d6d57a7c78d7e3c3d2ea536d6451f0
Sankha <sankha2812@gmail.com>
2021-08-27 12:31:13 +0530

    Completed snake expansion

--- snake.js
+++ snake.js
@@ -1,12 +1,11 @@
 import { getInputDirection } from "./input.js"
 
 export const SNAKE_SPEED = 5
-const snakeBody = [
-    { x: 11, y: 11 }
-]
+const snakeBody = [{ x: 11, y: 11 }]
+let newSegment=0
 
 export function update() {
-    // console.log('update snake')
+    addSegments()
     const inputDirection = getInputDirection()
     for(let i = snakeBody.length - 2; i>=0; i--){
         snakeBody[i+1] = {...snakeBody[i]}
@@ -25,4 +24,26 @@ export function draw(gameBoard) {
         snakeElement.classList.add('snake')
         gameBoard.appendChild(snakeElement)
     })
+}
+
+export function expandSnake(amount){
+    newSegment += amount
+}
+
+export function onSnake(position){
+    return snakeBody.some(segment => {
+        return equalPositions(segment, position)
+    })
+}
+
+function equalPositions(pos1, pos2){
+    return pos1.x === pos2.x && pos1.y ===pos2.y
+}
+
+function addSegments(){
+    for(let i=0; i< newSegment; i++){
+        snakeBody.push({ ...snakeBody[snakeBody.length - 1] })
+    }
+
+    newSegment=0
 }
